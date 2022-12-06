a6d843087388cca142e34ca020a1b52d2caec1eb
DylanSilby <dylanpsilby+github@gmail.com>
2022-12-06 21:55:09 +0000

    added Day 4
    Added the file and input for Day 4 (no solution)

---
+++ 2022/Day-4/main.py
@@ -0,0 +1,7 @@
+file = open("2022\\Day-4\\input.txt", "r+")
+Lines = file.readlines()
+file.close()
+
+exampleLines = ["2-4","6-8","2-3","4-5","5-7","7-9","2-8","3-7","6-6","4-6","2-6","4-8"]
+for i in Lines:
+    
